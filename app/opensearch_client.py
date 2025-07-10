9a58a02d77556cf3477cf106fc121ef651d0bd79
steve <steve.berenger@gmail.com>
2025-07-10 13:21:37 +0200

    update

--- app/opensearch_client.py
+++ app/opensearch_client.py
@@ -1,6 +1,7 @@
+import os
 from opensearchpy import OpenSearch
 
-client = OpenSearch(
-    hosts=[{"host": "localhost", "port": 9200}],
-    http_compress=True
-)
+def get_client():
+    host = os.getenv("OPENSEARCH_HOST", "localhost")
+    port = int(os.getenv("OPENSEARCH_PORT", 9200))
+    return OpenSearch([{"host": host, "port": port}])
