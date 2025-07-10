873f268cdb0ac486d327b30c4a428f9a982283fe
steve <steve.berenger@gmail.com>
2025-07-10 13:24:13 +0200

    update models

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from enum import Enum
+from time import timezone
 from pydantic import BaseModel, field_validator
 
 class LogLevel(str, Enum):
@@ -10,12 +11,12 @@ class LogLevel(str, Enum):
 
 class LogEntry(BaseModel):
     timestamp: datetime
-    level: str
+    level: LogLevel
     message: str
     service: str
     
     @field_validator("timestamp")
     def timestamp_must_be_in_past(cls, value):
-        if value > datetime.now():
+        if value > datetime.now(timezone.utc):
             raise ValueError("Timestamp must be in the past")
         return value
